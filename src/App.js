12ce833d1ad60404c009a5fde26dcc805dd2a034
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-28 16:00:14 -0300

    Adicionando provider com store para aplicacao

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import Routes from './routes';
+import { Provider } from 'react-redux';
+import store from './store';
+import Route from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Route />
+    </Provider>
+  );
 }
 
 export default App;
